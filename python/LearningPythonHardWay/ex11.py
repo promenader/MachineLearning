728253432a24252428224147186190fbc9ad6355
chenhaiping <chenhaiping@didichuxing.com>
2017-11-03 17:26:42 +0800

    python

---
+++ python/LearningPythonHardWay/ex11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How weight are you?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
